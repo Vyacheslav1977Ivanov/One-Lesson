32284771a712176ddbe349c286d5e332181f5dbf
Vyacheslav Ivanov <vya1977@mail.ru>
2023-10-26 22:01:38 +0300

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2=Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.Write("max="+num1);
+}
+else
+{
+    Console.Write("max="+num2);
+}
