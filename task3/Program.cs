cde6bc067a1d9914e5dffd29e9b42af4ece1ee3c
Vyacheslav Ivanov <vya1977@mail.ru>
2023-10-26 22:23:36 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+if(num!=0)
+{
+    int result=num%2;
+    if (result>0)
+    {
+        Console.Write("Нет");
+    }
+    else
+    {
+        Console.Write("Да");
+    }
+}
